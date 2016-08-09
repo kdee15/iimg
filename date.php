435bb3fa618422b3f1c86461afcec02e4649877f
Kirk Daniels <kdee15@gmail.com>
2016-08-10 00:48:59 +0200

    blog/compliance

--- date.php
+++ date.php
@@ -2,54 +2,30 @@
 /* template name: Posts by Category! */
 get_header(); ?>
 
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-
     <?php
 
-            // Willem se kak
-            if (is_category( )) {
-                $monthnum = get_query_var('monthnum');
-                $year = get_query_var('year');
-            }
+    // Willem se kak
+    if (is_category( )) {
+        $monthnum = get_query_var('monthnum');
+        $year = get_query_var('year');
+    }
 
-            // Make a header for the Date Archive
-            $date = strtotime($year . '-' . $monthnum . '-01 00:00:00');
-            $monthname = date('F', $date);
+    // Make a header for the Date Archive
+    $date = strtotime($year . '-' . $monthnum . '-01 00:00:00');
+    $monthname = date('F', $date);
 
-            echo "<h3 class='category-title'>".$monthname."</h3>";
+    echo "<h3 class='category-title'>".$monthname."</h3>";
 
-            // create a custom wordpress query
-            query_posts("monthnum=$monthnum&year=$year&posts_per_page=100");
+    // create a custom wordpress query
+    query_posts("monthnum=$monthnum&year=$year&posts_per_page=100");
 
-            // start the wordpress loop!
-            if (have_posts()) : while (have_posts()) : the_post(); ?>
+    // start the wordpress loop!
+    if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-            <?php // create our link now that the post is setup ?>
+    <?php // create our link now that the post is setup ?>
 
-                <?php the_title() ?>
+        <?php the_title() ?>
 
-            <?php endwhile; endif; // done our wordpress loop. Will start again for each category ?>
-
-
+    <?php endwhile; endif; // done our wordpress loop. Will start again for each category ?>
 
 <?php get_footer(); ?>
